432af2f98b32c8d77acd687c82080bf8e9786976
John Bradley <johnhavenbradley@gmail.com>
2018-03-19 09:34:31 -0700

    initial commit

---
+++ Star Wars Encyclopedia/Star Wars Encyclopedia/PeopleViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Star Wars Encyclopedia
+//
+//  Created by john bradley on 3/19/18.
+//  Copyright © 2018 john. All rights reserved.
+//
+
+import UIKit
+
+class PeopleViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
